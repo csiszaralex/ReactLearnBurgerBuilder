2e50c76cc25190ed6751a0bd1335171e335c7042
Alex <csiszaralex@gmail.com>
2021-04-02 20:08:45 +0200

    :sparkles: Done BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,13 @@
 import { Component } from 'react';
 import Burger from '../../components/burger/Burger';
+import BuildControls from '../../components/buildControls/BuildControls';
 
+const INGREDIENT_PRICE = {
+	salad: 0.5,
+	cheese: 0.4,
+	meat: 1.3,
+	bacon: 0.7,
+};
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
@@ -9,13 +16,49 @@ class BurgerBuilder extends Component {
 			cheese: 0,
 			meat: 0,
 		},
+		totalPrice: 4,
+	};
+
+	addIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		const updatedCount = oldCount + 1;
+		const updatedIngredients = { ...this.state.ingredients };
+		updatedIngredients[type] = updatedCount;
+		const priceAddition = INGREDIENT_PRICE[type];
+		const newPrize = this.state.totalPrice + priceAddition;
+		this.setState({
+			totalPrice: newPrize,
+			ingredients: updatedIngredients,
+		});
+	};
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if(oldCount <= 0) return;
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = { ...this.state.ingredients };
+		updatedIngredients[type] = updatedCount;
+		const priceDeduction = INGREDIENT_PRICE[type];
+		const newPrize = this.state.totalPrice - priceDeduction;
+		this.setState({
+			totalPrice: newPrize,
+			ingredients: updatedIngredients,
+		});
 	};
 
 	render() {
+		const disabledInfo ={...this.state.ingredients}
+		for (const key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0 
+		}
 		return (
 			<>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build Controll</div>
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+					price={this.state.totalPrice}
+				/>
 			</>
 		);
 	}
